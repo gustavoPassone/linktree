d67898f5b18f736acf194f31630b33115cb28a5d
Gustavo Martins Passone <gustavo.martins@grupoflamingo.com.br>
2025-10-14 18:28:15 -0300

    feat(theme): Tema claro e escuro funcionando

--- js/script.js
+++ js/script.js
@@ -1,2 +1,26 @@
-//ativar icones lucide
-lucide.createIcons();
+document.addEventListener('DOMContentLoaded', () => {
+    const themeToggle = document.getElementById('theme-toggle');
+    const body = document.body;
+    const lightIcon = themeToggle.querySelector('.light-icon');
+    const darkIcon = themeToggle.querySelector('.dark-icon');
+
+    //aplicar o tema
+    function applyTheme(isDarkMode) {
+        if (isDarkMode) {
+            body.classList.add('dark-mode');
+            lightIcon.style.display = 'none';
+            darkIcon.style.display = 'inline';
+        } else {
+            body.classList.remove('dark-mode');
+            lightIcon.style.display = 'inline';
+            darkIcon.style.display = 'none';
+        }
+    }
+
+    //mudar de tema
+    themeToggle.addEventListener('click', () => {
+        const isDarkMode = body.classList.contains('dark-mode');
+        //inverte o tema atual
+        applyTheme(!isDarkMode);
+    });
+});
